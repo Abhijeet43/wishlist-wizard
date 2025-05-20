fd005a78eccc5c1201c67051f0e77de2cb1697a0
Abhijeet <abhijeetsinha43@gmail.com>
2025-05-20 12:12:48 +0530

    setup: boiler plate react, daisy ui and react-router

---
+++ src/routes/index.js
@@ -0,0 +1,12 @@
+import { createBrowserRouter } from "react-router";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+  },
+  {
+    path: "/create-wishlist",
+  },
+]);
+
+export default router;
